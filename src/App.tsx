2e63760d146acbcc03f09e5910fde10f18b81cb8
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-07-17 12:57:22 +0000

    feat: urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import './App.css'
-import * as React from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDiscord, faGithub, faMastodon, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faBlog, faCircle } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
-  // OsuBeat :weary:
+  // sorry peppy for naming the variable Osu instead of osu! you know camel case stuff please don't kill me or ban me but at lease give me a plushie or something the taikonator one is really nice actually but who can't pass the offer of getting pippi lol she's so fucking cute
   const [osuBeat, setOsuBeat] = React.useState(false)
   return (
     <div>
       <div className='aboutMeBox'>
         <img src='https://github.com/SrIzan10.png' height='200px' />
-        <p>I'm a hobbyist developer and <a href='https://osu.ppy.sh/users/SrIzan10'>osu! player</a> based on Spain who loves to open-source and to work on teams.</p>
+        <p>I'm a hobbyist developer and osu! player based on Spain who loves to open-source and to work on teams.</p>
         <p>Stalk me on social media:</p>
         <div className='icons'>
-          <FontAwesomeIcon icon={faGithub} onDragOver={() => setOsuBeat(true)} beatFade={osuBeat} />
-          <FontAwesomeIcon icon={faGithub} />
+          <a href='https://github.com/SrIzan10'><FontAwesomeIcon icon={faGithub} /></a>
+          <a href='https://social.srizan.dev'><FontAwesomeIcon icon={faMastodon} /></a>
+          <a href='https://twitter.com/itssrizan'><FontAwesomeIcon icon={faTwitter} /></a>
+          <a href='https://srizan.dev/blog'><FontAwesomeIcon icon={faBlog} /></a>
+          <a href='https://discord.com/users/703974042700611634'><FontAwesomeIcon icon={faDiscord} /></a>
+          <a href='https://osu.ppy.sh/users/25350735'><FontAwesomeIcon icon={faCircle} onMouseEnter={() => setOsuBeat(true)} onMouseLeave={() => setOsuBeat(false)} beatFade={osuBeat} /></a>
         </div>
       </div>
     </div>
