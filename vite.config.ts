db8d35536820f22900545a5046bb3b64021feda6
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-08-23 15:38:26 +0200

    chore: forgot to remove that rollup thing there

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx({ baseUrl: '/blog' })],
+  plugins: [react()],
   server: {
     port: 3000
   }
