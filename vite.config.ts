0655a6eff3bc84cd924dddc0b070c98db9d21322
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-10-09 15:28:01 +0200

    chore: nah nvm

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), compression({ algorithm: 'brotliCompress', deleteOriginalAssets: true, skipIfLargerOrEqual: true,  })],
+  plugins: [react()],
   server: {
     port: 3000
   },
