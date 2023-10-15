b4e3a135f8010953eb175bbd0471785be3f46487
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-10-15 14:36:02 +0200

    feat: analytics

--- src/BlogNavBar.tsx
+++ src/BlogNavBar.tsx
@@ -7,7 +7,7 @@ export function BlogNavBar(props: Props) {
                 <img src="https://github.com/SrIzan10.png" alt="main profile picture" height="50vh" />
                 <p>{props.title || 'Sr Izan\'s blog'}</p>
             </div>
-            <a href={'/'} className="backHomeLink">Go back home</a>
+            <a href={props.title ? '/blog' : '/'} className="backHomeLink">Go back {props.title ? 'to posts' : 'home'}</a>
         </div>
     )
 }
