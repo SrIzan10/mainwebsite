b2e7ffb670828ac8bda3e865687224c7cf76da15
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2024-05-25 16:09:59 +0200

    feat: move to astro (#10)
    * initial commit
    
    * add shadcn ui
    
    * only main page is left!
    
    * ready to pr?
    
    * change font

---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly BLOG_CONTENT_KEY: string;
+    readonly BLOG_CONTENT_URL: string;
+}
