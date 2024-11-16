614c087aa8dc7d42de257f518eabeeda3767cd82
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2024-11-16 10:49:44 +0100

    feat: change links and nuke blog

--- src/lib/ghost.ts
+++
@@ -1,8 +0,0 @@
-import GhostContentAPI from '@tryghost/content-api';
-
-// Create API instance with site credentials
-export const ghostClient = new GhostContentAPI({
-    url: import.meta.env.BLOG_URL, // This is the default URL if your site is running on a local environment
-    key: import.meta.env.BLOG_CONTENT_KEY,
-    version: 'v5.82',
-});
