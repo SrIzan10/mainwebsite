de0efa285da4ceabc2a0c3be87ba4938fd06c562
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-05-23 19:55:41 +0200

    initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,55 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+import type { IconMap, SocialLink, Site } from '@/types'
 
-export const SITE_TITLE = 'Sr Izan\'s website';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE: Site = {
+  title: 'astro-erudite',
+  description:
+    'astro-erudite is a opinionated, unstyled blogging template—built with Astro, Tailwind, and shadcn/ui.',
+  href: 'https://astro-erudite.vercel.app',
+  author: 'jktrn',
+  locale: 'en-US',
+  featuredPostCount: 2,
+  postsPerPage: 3,
+}
+
+export const NAV_LINKS: SocialLink[] = [
+  {
+    href: '/blog',
+    label: 'blog',
+  },
+  {
+    href: '/authors',
+    label: 'authors',
+  },
+  {
+    href: '/about',
+    label: 'about',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    href: 'https://github.com/jktrn',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://twitter.com/enscry',
+    label: 'Twitter',
+  },
+  {
+    href: 'mailto:jason@enscribe.dev',
+    label: 'Email',
+  },
+  {
+    href: '/rss.xml',
+    label: 'RSS',
+  },
+]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-    readonly BLOG_CONTENT_KEY: string;
-    readonly BLOG_CONTENT_URL: string;
-}
