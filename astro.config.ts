3cf91b4f7222b7419b8059d5d6a39ceb07bab5e3
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-09-25 21:24:51 +0200

    chore: testing post and cloudflare ssr

--- astro.config.ts
+++ astro.config.ts
@@ -19,8 +19,11 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://astro-erudite.vercel.app',
+
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
@@ -70,16 +73,20 @@ export default defineConfig({
     sitemap(),
     icon(),
   ],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   server: {
     port: 1234,
     host: true,
   },
+
   devToolbar: {
     enabled: false,
   },
+
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
@@ -110,4 +117,9 @@ export default defineConfig({
     ],
     remarkPlugins: [remarkMath, remarkEmoji],
   },
+
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
+  output: 'server',
 })
