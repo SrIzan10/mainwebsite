1c4d70c11069c837ab37d9274a363d306a9d1337
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-09-26 07:52:40 +0200

    fix: cloudflare deployments

--- astro.config.ts
+++ astro.config.ts
@@ -76,6 +76,11 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: import.meta.env.PROD ? {
+        "react-dom/server": "react-dom/server.edge",
+      } : undefined,
+    },
   },
 
   server: {
